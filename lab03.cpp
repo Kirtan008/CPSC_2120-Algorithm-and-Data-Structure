d1567ce122c8993bf565623f4d5c7272505f8c1c
Kirtan008 <45375346+Kirtan008@users.noreply.github.com>
2019-11-04 18:15:18 -0500

    Update lab03.cpp

--- lab03.cpp
+++ lab03.cpp
@@ -8,18 +8,15 @@ Name: Kirtan Patel
 #include <math.h>
 using namespace std;
 
-class point
-{
+class point{
   double x, y;
 };
 
-Table::Table()
-{
+Table::Table(){
 
 }
 
-Table::Table(int s)
-{
+Table::Table(int s){
   size = s;
   table = new Node**[size];
   for (int i = 0; i < size; ++i)
@@ -32,8 +29,7 @@ Table::Table(int s)
   }
 }
 
-Table::~Table()
-{
+Table::~Table(){
   for (int i=0; i < size; i++)
   {
       for(int j = 0; j < size; j++)
@@ -50,66 +46,46 @@ Table::~Table()
   delete[] table;
 }
 
-int Table::myhashX(Point p)
-{
+int Table::myhashX(Point p){
   int i = 1;
   int new_x = i*(p.x * size);
   return new_x;
 }
 
-int Table::myhashY(Point p)
-{
+int Table::myhashY(Point p){
   int i = 1;
   int new_y = i*(p.y * size);
   return new_y;
 }
 
-void Table::read()
-{
+void Table::read(){
   ifstream input("points.txt");
-
   Point p;
-
-  while(input >> p.x >> p.y)
-  {
+  while(input >> p.x >> p.y){
     int i = myhashX(p);
-    int j = myhashY(p);
-    {
-      table[i][j] = new Node(p, table[i][j]);
     }
   }
 }
 
-double Table::distance(Point p1, Point p2)
-{
+double Table::distance(Point p1, Point p2){
   return sqrt((p1.x-p2.x)*(p1.x-p2.x) + (p1.y-p2.y)*(p1.y-p2.y));
 }
 
-double Table::closestDist()
-{
-
+double Table::closestDist(){
   double currDist = 1;
-
-  for(int i = 0; i < size; i++)
-  {
-    for(int j = 0; j < size; j++)
-    {
+  for(int i = 0; i < size; i++){
+    for(int j = 0; j < size; j++){
       Node* temp = table[i][j];
-      while(temp != NULL)
-      {
-        for(int k = i-1; k < i+2; k++)
-        {
-          for(int l = j-1; l < j+2;l++)
-          {
+      while(temp != NULL){
+        for(int k = i-1; k < i+2; k++){
+          for(int l = j-1; l < j+2;l++){
             if(k < 0 || l < 0) continue;
             if(k > size-1 || l > size-1) continue;
             Node* curr = table[k][l];
-            while(curr != NULL)
-            {
+            while(curr != NULL){
               if(curr == temp) {curr = curr->next; continue;}
               double dist = distance(curr->getPoint(), temp->getPoint());
-              if(dist < currDist)
-              {
+              if(dist < currDist){
                 currDist = dist;
               }
               curr = curr->next;
@@ -123,8 +99,7 @@ double Table::closestDist()
   return currDist;
 }
 
-int main()
-{
+int main(){
   Table t(2000);
   t.read();
   cout << t.closestDist() << endl;
